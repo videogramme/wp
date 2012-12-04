81d5b2efc0b16fe41823bd67032d2abc710f0088
videogramme <videogramme@gmail.com>
2012-12-04 17:24:13 +0100

    wpml

--- wp-content/plugins/isotope-visual-post-layouts/inc/myfile.php
+++
@@ -1,9 +0,0 @@
-<!-- damien was - if you change this please help me and MetaFizzy by buying a commercial licence -->					
-	<script type="text/javascript">
-	jQuery(document).ready(function() {
-    var mycontainer = jQuery('#isocontent');
-      mycontainer.isotope({
-      itemSelector: '.box'
-      });
-    });
-   </script>
